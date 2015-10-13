48c88ad6ee9d078d85670ea6dee7b1dfd69f616b
Jooho Lee <ljhiyh@gmail.com>
2015-10-13 15:14:52 +0900

    update RHEV storage script. This is first full version

--- rhev_ansible/copy.sh
+++ rhev_ansible/copy.sh
@@ -1,1 +1,2 @@
-scp rhel-server-6.6-x86_64-150304.iso root@10.211.55.9:/root/
+scp /Users/jhouse/dev/files/rhel-server-6.6-x86_64-150304.iso root@10.211.55.9:/mnt/nfs/iso/rhel-server-6.6-x86_64-150304.iso
+scp /Users/jhouse/dev/files/rhev-3.5.0.1-150304.iso root@10.211.55.9:/mnt/nfs/iso/rhev-3.5.0.1-150304.iso
